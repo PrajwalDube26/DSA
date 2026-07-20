55cf2052fd46ebcfbe93a3fc7df1afb4b3e7b822
PrajwalDube26 <prajwaldube26@gmail.com>
2026-07-20 17:40:32 +0530

    Time: 0 ms (100%), Space: 12.7 MB (76.35%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,34 +2,35 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,int>um;
-        for(int i=0;i<nums2.size();i++)
+        stack<int>s1;
+        s1.push(nums2[0]);
+
+        for(int i=1;i<nums2.size();i++)
         {
-            um[nums2[i]]=i;
+            while(!s1.empty() && s1.top()<nums2[i])
+            {
+                um[s1.top()] = nums2[i];
+                s1.pop();
+            }
+            s1.push(nums2[i]);
         }
 
-        vector<int>ans(nums1.size(),-1);
+        while(!s1.empty())
+        {
+            um[s1.top()] = -1;
+            s1.pop();
+        }
 
+        vector<int>ans(nums1.size());
         for(int i=0;i<nums1.size();i++)
         {
-            auto it  = um.find(nums1[i]);
-
+            auto it = um.find(nums1[i]);
             if(it != um.end())
             {
-                int j = it->second;
-                int a= nums2[j];
-
-                for(int k=j+1;k<nums2.size();k++)
-                {
-                    if(nums2[k]>a)
-                    {
-                        ans[i]=nums2[k];
-                        break;
-                    }
-                }
+                ans[i] = it->second;
             }
         }
 
         return ans;
-
     }
 };
