122dc7a2d2f67581d362df7692fc682d4be8e2d3
PrajwalDube26 <prajwaldube26@gmail.com>
2026-07-29 02:17:30 +0530

    Time: 0 ms (100%), Space: 8.4 MB (61.72%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,37 +1,23 @@
-#include<iostream>
-using namespace std;
-
 class Solution 
 {
 public:
     int maxDepth(string s) 
     {
-        int count=0,i=0;
-        int A[100];
+        int count=0,ans=0;
 
         for(char c:s)
         {
             if(c=='(')
             {
                 count++;
-
             }
             else if(c==')')
             {
-                A[i++]=count;
                 count--;
             }
+            ans = max(ans,count);
         }
-
-        int ans=0;
-
-        for(int j=0;j<i;j++)
-        {
-            ans = max(ans,A[j]);
-        }
-
-        return ans;
         
+        return ans;
     }
 };
-
