403564099f32612d19f22f521383e85e0660ddda
PrajwalDube26 <prajwaldube26@gmail.com>
2026-07-30 18:52:41 +0530

    Time: 0 ms (100%), Space: 14.2 MB (45.64%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int minOperations(vector<string>& logs) {
         int n=logs.size();
-        stack<string>st;
-        int ans=0;
+        // stack<string>st;
+        int ans=0,count=0;
 
         for(int i=0;i<n;i++)
         {
@@ -13,23 +13,28 @@ public:
             }
             else if(logs[i]=="../")
             {
-                if(!st.empty())
+                // if(!st.empty())
+                // {
+                //     st.pop();
+                // }
+                if(count!=0)
                 {
-                    st.pop();
+                    count--;
                 }
             }
             else
             {
-                st.push(logs[i]);
+                // st.push(logs[i]);
+                count++;
             }
         }
 
-        while(!st.empty())
-        {
-            st.pop();
-            ans++;
-        }
+        // while(!st.empty())
+        // {
+        //     st.pop();
+        //     ans++;
+        // }
 
-        return ans;
+        return count;
     }
 };
