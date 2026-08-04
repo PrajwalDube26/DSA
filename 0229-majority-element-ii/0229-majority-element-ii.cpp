cab8d08e1e7edb563285ae94d63591827396b89c
PrajwalDube26 <prajwaldube26@gmail.com>
2026-08-04 18:44:19 +0530

    Time: 0 ms (100%), Space: 23.9 MB (41.35%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,24 +1,64 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n=int(nums.size() / 3) + 1;;
-        vector<int>v1; 
-        unordered_map<int,int>m1;
-        for(int i=0;i<nums.size();i++)
+        int n = nums.size();
+        vector<int>ans;
+        int needed = (n/3)+1;
+        int cnt1=0,ma1=INT_MIN;
+        int cnt2=0,ma2=INT_MIN;
+
+        for(int i=0;i<n;i++)
         {
-            m1[nums[i]]++;
-            
-            if(m1[nums[i]] == n)
+            if(cnt1==0 && ma2!=nums[i])
             {
-                v1.push_back(nums[i]);
+                cnt1=1;
+                ma1=nums[i];
+            }
+            else if(cnt2==0 && ma1!=nums[i])
+            {
+                cnt2=1;
+                ma2=nums[i];
+            }
+            else if(ma1 == nums[i])
+            {
+                cnt1++;
+            }
+            else if(ma2 == nums[i])
+            {
+                cnt2++;
+            }
+            else
+            {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        
+        cnt1=0,cnt2=0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==ma1)
+            {
+                cnt1++;
             }
 
-            if(v1.size()==2)
+            if(nums[i]==ma2)
             {
-                break;
+                cnt2++;
             }
         }
 
-        return v1;
+        if(cnt1>=needed)
+        {
+            ans.push_back(ma1);
+        }
+
+        if(cnt2>=needed)
+        {
+            ans.push_back(ma2);
+        }
+
+        return ans;
     }
 };
