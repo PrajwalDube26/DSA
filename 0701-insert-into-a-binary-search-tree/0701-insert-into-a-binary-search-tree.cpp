3dc96e5735fcd581a74dce76395c071e53730cc1
PrajwalDube26 <prajwaldube26@gmail.com>
2026-08-07 19:24:54 +0530

    Time: 0 ms (100%), Space: 59.5 MB (26.05%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -11,52 +11,49 @@
  */
 class Solution {
 public:
-    TreeNode* insertIntoBST(TreeNode* root, int val) {
-        int ans=INT_MAX;
-        TreeNode* temp = root;
-        TreeNode* ceil=NULL;
-        
-        while(temp!=NULL)
-        {
-            if(temp->val > val)
-            {
-                if(temp->val < ans)
-                {
-                    ans = temp->val;
-                    ceil = temp;
-                }
-                temp =temp->left;
-            }
-            else if(temp->val < val)
-            {
-                temp =temp->right;
-            }
-            
-        }
-        
-        if(ceil == NULL)
-        {
-            TreeNode* ans= new TreeNode(val);
-            ans->left = root;
-            return ans;
-        }
+    void insert(TreeNode* root, int val) 
+    {
+        TreeNode* temp=root;
 
-        TreeNode* temp2 = ceil->left;
-        ceil->left = new TreeNode(val);
+        if(temp!=NULL)
+        {
+            if(temp->val < val)
+            {
+                if(temp->right == NULL)
+                {
+                    temp->right = new TreeNode(val);
+                    return;
+                }
+                else
+                {
+                    insertIntoBST(root->right,val);
+                }
+                
+            }
+            else if(temp->val > val)
+            {
+                if(temp->left == NULL)
+                {
+                    temp->left = new TreeNode(val);
+                    return;
+                }
+                else
+                {
+                    insertIntoBST(root->left,val);
+                }
+            }
+        }
+    }
+
+    TreeNode* insertIntoBST(TreeNode* root, int val) 
+    {
+        if(root == NULL)
+        {
+            return new TreeNode(val);
+        }
         
-        if(temp2==NULL)
-        {
-            return root;
-        }
-        else if(temp2->val > val)
-        {
-            ceil->left->right = temp2;
-        }
-        else if(temp2->val < val)
-        {
-            ceil->left->left = temp2;
-        }
-        
+        insert(root,val);
+
         return root;
     }
 };
