d7e74c60592e58f166591e62baef85c1c3ff340b
PrajwalDube26 <prajwaldube26@gmail.com>
2026-08-28 19:56:27 +0530

    Time: 382 ms (96.2%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,7 +1,13 @@
+-- select w1.id AS id
+-- from Weather w1
+-- where w1.temperature > (
+--     select w2.temperature
+--     from Weather w2
+--     where w2.recordDate = DATE_SUB(w1.recordDate , INTERVAL 1 DAY)
+-- );
+
 select w1.id AS id
 from Weather w1
-where w1.temperature > (
-    select w2.temperature
-    from Weather w2
-    where w2.recordDate = DATE_SUB(w1.recordDate , INTERVAL 1 DAY)
-);
+JOIN Weather w2
+ON w1.recordDate = DATE_add(w2.recordDate, INTERVAL 1 DAY)
+WHERE w2.temperature < w1.temperature;
