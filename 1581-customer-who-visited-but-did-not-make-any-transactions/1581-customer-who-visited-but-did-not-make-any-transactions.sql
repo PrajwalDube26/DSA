2f51621064b196c9fd8bb4c0cf3f3ea7992102e3
PrajwalDube26 <prajwaldube26@gmail.com>
2026-09-01 21:46:25 +0530

    Time: 1552 ms (46.41%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -12,5 +12,4 @@ FROM Visits v
 LEFT JOIN Transactions t
 ON v.visit_id=t.visit_id
 WHERE t.transaction_id IS NULL
-GROUP BY v.customer_id
-HAVING COUNT(v.customer_id) IS NOT NULL;
+GROUP BY v.customer_id;
