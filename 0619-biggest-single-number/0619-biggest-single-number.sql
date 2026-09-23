4c6ba6b4aac0b964ff46145c4c7da0b183f96ee7
PrajwalDube26 <prajwaldube26@gmail.com>
2026-09-23 21:16:37 +0530

    Time: 497 ms (42.16%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,5 +1,7 @@
 select MAX(t.num) AS num
-from (select num
-from MyNumbers
-group by num
-HAVING COUNT(*)=1) t;
+from (
+    select num
+    from MyNumbers
+    group by num
+    HAVING COUNT(*)=1
+    ) t;
